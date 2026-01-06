f64a1a0f4b4acf43adbe7d981d7a826c77e0e74f
Ratnesh Singh <ratnesh.sheatvns@gmail.com>
2026-01-06 16:53:05 +0530

    Spring Security 1.5

--- src/main/java/com/finance/concierge/config/SecurityConfig.java
+++ src/main/java/com/finance/concierge/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000")); // Allow frontend origins
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000","https://ai.newagetechnology.in")); // Allow frontend origins
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
